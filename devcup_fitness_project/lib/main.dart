d92449358c0da155e000defceb771ef7f8414fa7
Ruchika Suryawanshi <ruchikasuryawanshi710@gmail.com>
2022-09-27 09:51:56 +0530

    UI and model integrated

--- devcup_fitness_project/lib/main.dart
+++ devcup_fitness_project/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'exercise/freakyPlans.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FreakyPlans(),
     );
   }
 }
